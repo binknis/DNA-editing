56ce6365e5b767b67e13f702ec619b9072f3992c
BinyaminKnisbacher <binyamin.knisbacher@biu.ac.il>
2017-11-06 14:30:46 +0200

    Remove non-generic files
    Files from previous analysis.
    The CreateTrackFiles .sh files could be useful to imitate in the future.

--- Perl_DNAE/CreateTrackDir_sh_files/createTrackDirs.reg.LowAndHighConf.GA.Human.ERVK.sh
+++
@@ -1,2 +0,0 @@
-perl516 analysis_scripts/createTrackFiles2.pl Human LTR ERVK 1e-0 5 GA "clusters|1|/home/alu/binknis/Rscripts/DNAE/Choose_best_params/lowConfidence/Clusters/clusters_GA_Human.tab|lowConfidence_byClusters"
-perl516 analysis_scripts/createTrackFiles2.pl Human LTR ERVK 1e-0 5 GA "clusters|1|/home/alu/binknis/Rscripts/DNAE/Choose_best_params/highConfidence/Clusters/clusters_GA_Human.tab|highConfidence_byClusters"
--- Perl_DNAE/runBMMForg.pl
+++
@@ -1,8 +0,0 @@
-use strict;
-use getAll; 
-use lib "/home/alu/binknis/Perl_DNAE/analysis_scripts"; 
-use analysisSubs;
-
-(my $org, my $class, my $out) = @ARGV; 
-
-analysisSubs::calcBMMForg($org, $class, $out); 
